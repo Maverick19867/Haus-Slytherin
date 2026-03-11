76f0a0a841552eade3939fb8217d6a6a50fb1adb
Andreas Lorenz <andreas.lorenz1986@web.de>
2026-03-11 09:08:41 +0100

    Initial commit

---
+++ Aufgabe1.cs
@@ -0,0 +1,19 @@
+/* 1.1 Erkläre den Unterschied zwischen imperativ, objektorientiert und funktional in 2 Sätze7
+Imperativ beschreibt die Programmierung als eine Abfolge von Anweisungen, 
+während objektorientiert die Organisation von Code in Objekten und Klassen betont. 
+Funktional hingegen konzentriert sich auf die Verwendung von Funktionen als erste Bürger und vermeidet Zustandsänderungen. */
+
+/* 1.2 Ordne folgende Probleme einem ParadigmaZU: GUI Anwedung Datenpipeline und Spiel.
+GUI Anwendung - Objektorientiert
+Datenpipeline - Funktional
+Spiel - Objektorientiert */
+
+/* 1.3 Erkläre warum OOP für große System sinnvoll ist.
+OOP ermöglicht die Kapselung von Daten und Funktionen in Klassen, was die Wartbarkeit und Wiederverwendbarkeit von Code verbessert.
+Es erleichtert die Zusammenarbeit in Teams, da verschiedene Entwickler an verschiedenen Klassen arbeiten können, ohne sich gegenseitig zu stören. */
+
+/*1.4 Finde ein Beispiel aus der realen Welt für OOP.
+Ein Beispiel für OOP in der realen Welt ist ein Auto. Ein Auto kann als Objekt betrachtet werden, 
+das Eigenschaften wie Farbe, Marke und Modell hat, sowie Methoden wie Starten, Beschleunigen und Bremsen.
+Verschiedene Autos können unterschiedliche Werte für diese Eigenschaften haben, aber sie teilen die gleichen Methoden. 
+ Was die Wiederverwendbarkeit von Code ermöglicht. */
